c7a2548c19006823b1d801c3b44c757bda9b6377
jolome777 <77jonas77@gmail.com>
2018-06-21 15:30:11 +0100

    Añadida resto de funcionalidades salvo pintar gráficos con QT

--- qt_script.py
+++ qt_script.py
@@ -62,24 +62,23 @@ class Ventana(QtWidgets.QWidget):
         self.layoutMenu.addWidget(self.buttonEntrenar)
         #Layout donde irían los resultados
         self.layoutWidget =QVBoxLayout()
-
-        self.infoLayout = QVBoxLayout()
+        self.infoLayout = QVBoxLayout() #Layout que muestra todos los widgets de mostrar un sólo tweet
 
         #Variables para mostrar un sólo tweet
-        self.tituloLabel = QLabel(self)
-        self.layoutWidget.addWidget(self.tituloLabel)
-        self.tweetLabel = QLabel(self)
+        self.tituloLabel = QLabel(self) #Etiqueta que muestra el título
+        self.infoLayout.addWidget(self.tituloLabel)
+        self.tweetLabel = QLabel(self) #Etiqueta que muestra el texto del tweet
         self.infoLayout.addWidget(self.tweetLabel)
-        self.nerLabel = QLabel(self)
+        self.nerLabel = QLabel(self) #Etiqueta que muestra el título de NER
         self.infoLayout.addWidget(self.nerLabel)
-        self.nonerLabel = QLabel(self)
+        self.nonerLabel = QLabel(self) #Etiqueta que muestra el aviso de que no hay NER
         self.infoLayout.addWidget(self.nonerLabel)
-        self.tabla = QTableWidget()
+        self.tabla = QTableWidget() #Tabla que muestra los resultados del NER
         self.infoLayout.addWidget(self.tabla)
-        self.claLabel = QLabel(self)
+        self.claLabel = QLabel(self) #Etiqueta que muestra el título que precede a la tabla de sentimientos
         self.infoLayout.addWidget(self.claLabel)
-        self.tablaSent = QTableWidget()
-        self.infoLayout.addWidget(self.tablaSent)
+        self.tablaSent = QTableWidget() #Tabla que muestra los sentimientos de cada algoritmo
+        self.infoLayout.addWidget(self.tablaSent) 
         #Variables para mostrar en entrenamiento
         self.entrenamientoLabel = QLabel(self)
         #Variables para la barra de progreso
@@ -96,6 +95,7 @@ class Ventana(QtWidgets.QWidget):
         self.consultaLayout.addWidget(self.buttonConsulta)
         self.consultaText = ""
         self.consultaTweet = 0
+        self.nerCantidadValor = 0
         # Conectar botón de consulta
         self.connect(self.buttonConsulta, QtCore.SIGNAL("clicked()"), self, QtCore.SLOT("guardarTexto()"))
 
@@ -115,6 +115,7 @@ class Ventana(QtWidgets.QWidget):
         #Diálogo para configurar parámetros de bloque de Twitter
         self.dialogConsulta = QInputDialog(self)
         self.dialogTweets = QInputDialog(self)
+        self.nerCantidad = QInputDialog(self)
         
 
         # Conectar a analizarUnTweet:
@@ -123,29 +124,38 @@ class Ventana(QtWidgets.QWidget):
         self.buttonEntrenar.clicked.connect(self.entrenar_algoritmos)
         # Conectar a cargar datos de Twitter
         self.buttonBloqueTwitter.clicked.connect(self.cuadroDialogo)
+        # Conectar a cargar datos de archivo
+        self.buttonBloqueFile.clicked.connect(self.analizarDeArchivo)
+
+    def limpiarLayout(self,layout):
+        for i in reversed(range(layout.count())): 
+            layout.itemAt(i).widget().hide()
+    def mostrarLayout(self,layout):
+        for i in range(layout.count()): 
+            layout.itemAt(i).widget().show()
 
     def cuadroDialogo(self):
         self.consultaText = self.dialogConsulta.getText(self,"Consulta de Twitter", "¿Sobre qué quieres buscar?")
         print(self.consultaText[0])
         self.consultaTweets = self.dialogTweets.getInt(self,"Cuántos twits quieres usar","La cantidad se multiplica por 100")
-        print(self.consultaTweet)
+        print(self.consultaTweets)
+        self.nerCantidadValor = self.nerCantidad.getInt(self,"Cuántos twits quieres usar en NER","Tweets que se usarán para NER")
+        print(self.nerCantidadValor)
         self.cargar_datos_de_twitter()
-
-
-
-    '''
-    Función para cargar datos de twitter directamente, lo almacena en una base de datos
+    '''Función para cargar datos de twitter directamente, lo almacena en una base de datos
     y lo devuelve en un dataframe. Se usa sólo para ver los resultados. No para entrenar.
     Estaría bien poder pasarle a la función el número de tweets que queremos buscar 
-    por cada iteración, el número de iteraciones y la búsqueda que queremos hacer.
-    '''
+    por cada iteración, el número de iteraciones y la búsqueda que queremos hacer.'''
     def cargar_datos_de_twitter(self):
+        self.limpiarLayout(self.infoLayout)
         consumer_key='ynSB0dFvqPl3xRU7AmYk39rGT'
         consumer_secret='6alIXTKSxf0RE57QK3fDQ8dxdvlsVr1IRsHDZmoSlMx96YKBFD'
         access_token='966591013182722049-BVXW14Hf5s6O2oIwS3vtJ3S3dOsKLbY'
         access_token_secret='829DTKPjmwsSytmp1ky9fMCJkjV0LZ04TbL9oqHGV6cDm'
         #parámetros de la consulta
         q = self.consultaText[0] + ' -filter:retweets AND -filter:replies'
+        #q = 'premier league -filter:retweets AND -filter:replies'
+        print(q)
         url = 'https://api.Twitter.com/1.1/search/tweets.json'
         pms = {'q' : q, 'count' : 100, 'lang' : 'en', 'result_type': 'recent'} 
         auth = OAuth1(consumer_key, consumer_secret, access_token,access_token_secret)
@@ -157,7 +167,7 @@ class Ventana(QtWidgets.QWidget):
         collection = db[collection_name]
         #Paginación (carga de 100 en 100 datos)
         pages_counter = 0
-        number_of_pages = self.consultaTweet
+        number_of_pages = self.consultaTweets[0]
         while pages_counter < number_of_pages:
             pages_counter += 1
             res = requests.get(url, params = pms, auth=auth)
@@ -177,7 +187,7 @@ class Ventana(QtWidgets.QWidget):
         df2 = pd.DataFrame(data=df['text'][-100:])
         dfNER = pd.DataFrame(data=df['text'])
         dfNER = self.tokenizar(dfNER)
-        anNER = dfNER['final'][-5:] #saca sólo los últimos 5
+        anNER = dfNER['final'][-self.nerCantidadValor[0]:] #saca sólo los últimos 5
         resultadoNER = self.usar_NER(anNER,3)
         for ent in resultadoNER:
             for it in ent:
@@ -189,17 +199,52 @@ class Ventana(QtWidgets.QWidget):
                 test_vectors = self.vectorizer.transform(test_data)
                 self.mostrar_graph(self.predecir_Naive_Bayes(test_vectors, it),self.predecir_SVC(test_vectors, it), self.predecir_KNN(test_vectors, it), self.predecir_MLP(test_vectors, it))
                 df2 = pd.DataFrame(data=df['text'])#vuelve a recargar el df3
-
-
+        self.progresLabel.setText("FINALIZADO")
+    '''Función que analiza twits sacados de un archivo a elegir por el usuario'''
+    def analizarDeArchivo(self):
+        filename = self.dialogo1.getOpenFileName(self, "Selecciona el fichero a analizar","/")
+        filename = filename[0].split("/")
+        filename = filename[-1]
+        self.nerCantidadValor = self.nerCantidad.getInt(self,"Cuántos twits quieres usar en NER","Tweets que se usarán para NER")
+        self.progressBarUnTweet.reset()
+        self.progressBarUnTweet.setMaximum(100)
+        self.progressBarUnTweet.setMinimum(0)
+        #filename = input("\tEscribe el nombre del fichero donde se encuentra el bloque de tweets: ") or 'bloque.csv'
+        dataset = pd.read_csv(filename)
+        df3 =pd.DataFrame(data=dataset['text'])
+        dfNER = pd.DataFrame(data=dataset['text'])
+        self.progressBarUnTweet.setValue(20)
+        self.progresLabel.setText("Tokenizando")
+        dfNER = self.tokenizar(dfNER)
+        anNER = dfNER['final'][-self.nerCantidadValor[0]:]
+        self.progressBarUnTweet.setValue(30)
+        resultadoNER = self.usar_NER(anNER,3)
+        self.progressBarUnTweet.reset()
+        self.progressBarUnTweet.setMaximum(100)
+        self.progressBarUnTweet.setMinimum(0)
+        self.progressBarUnTweet.setValue(90)
+        self.progresLabel.setText("Dibujando gráficos")
+        for ent in resultadoNER:
+            for it in ent:
+                print(it)
+                df3 = df3[df3['text'].str.contains(it[0])]
+                df3 = self.tokenizar(df3)
+                test_data = df3['final'][-100:] #saca sólo los últimos 100
+                test_data = list(test_data.apply(' '.join))
+                test_vectors = self.vectorizer.transform(test_data)
+                self.mostrar_graph(self.predecir_Naive_Bayes(test_vectors, it),self.predecir_SVC(test_vectors, it), self.predecir_KNN(test_vectors, it), self.predecir_MLP(test_vectors, it))
+                df3 =pd.DataFrame(data=dataset['text'])#vuelve a recargar el df3
+        self.progressBarUnTweet.setValue(100)
+        self.progresLabel.setText("FINALIZADO")
+    '''Función que analiza un tweet individual sacado de twitter.com'''
     def analizarUnTweet(self):
         #self.layoutWidget.insertLayout(2,self.infoLayout)
         #Barra de progreso
         #self.infoLayout.show()
+        self.consultaText = self.dialogConsulta.getText(self,"Consulta de Twitter", "¿Sobre qué quieres buscar?")
         self.progressBarUnTweet.reset()
         self.progressBarUnTweet.setMaximum(10)
         self.progressBarUnTweet.setMinimum(0)
-
-        
         #self.layoutProgressBar.addWidget(self.progressBarUnTweet)
         #Claves y tokens de la cuenta de twitter
         consumer_key='ynSB0dFvqPl3xRU7AmYk39rGT'
@@ -209,7 +254,7 @@ class Ventana(QtWidgets.QWidget):
         self.progressBarUnTweet.setValue(1)
         self.progresLabel.setText("Estableciendo claves de acceso")
         #parámetros de la consulta
-        q = 'premier league -filter:retweets AND -filter:replies'
+        q = self.consultaText[0] + ' -filter:retweets AND -filter:replies'
         url = 'https://api.Twitter.com/1.1/search/tweets.json'
         pms = {'q' : q, 'count' : 10, 'lang' : 'en', 'result_type': 'recent'} 
         auth = OAuth1(consumer_key, consumer_secret, access_token,access_token_secret)
@@ -359,10 +404,9 @@ class Ventana(QtWidgets.QWidget):
         # [JMTORRES] ojo con lo que haces aquí. Si consume mucho tiempo de CPU la aplicación
         # parecerá bloqueada. Eso habría que tenerlo en cuenta si pasa.
         self.layoutWidget.addLayout(self.infoLayout)
-    '''
-    Función que tokeniza los datos de un tweet, eliminando las stopwords y los carácteres
-    especiales
-    '''
+        self.mostrarLayout(self.infoLayout)
+    '''Función que tokeniza los datos de un tweet, eliminando las stopwords y los carácteres
+    especiales'''
     def tokenizar(self, df):
         #TOKENIZATION inicial para NER
         df['tokens'] = df['text'].apply(TweetTokenizer().tokenize)
@@ -375,19 +419,15 @@ class Ventana(QtWidgets.QWidget):
         df['digits'] = df['punctuation'].apply(lambda x: [i for i in x if i[0] not in list(string.digits)])
         df['final'] = df['digits'].apply(lambda x: [i for i in x if len(i) > 1])
         return df
-    '''
-    Función que recibe un dataframe con tweets de twitter y los deja preparados para
-    ser analizados.
-    '''
+    '''Función que recibe un dataframe con tweets de twitter y los deja preparados para
+    ser tokenizados'''
     def limpieza_de_datos_de_twitter(self, df):
         df['tweet_source'] = df['source'].apply(lambda x: BeautifulSoup(x).get_text())
         devices = list(set(df[df['tweet_source'].str.startswith('Twitter')]['tweet_source']))
         #devices.remove('Twitter Ads')
         df = df[df['tweet_source'].isin(devices)]
         return df
-    '''
-    Funciones de predicción de los diferentes algoritmos para los diferentes modelos
-    '''
+    '''Funciones de predicción de los diferentes algoritmos para los diferentes modelos'''
     def predecir_Naive_Bayes(self, test_vectors, it):
         mod = MultinomialNB()
         file = open('NaiveBayes', 'rb')
@@ -428,6 +468,7 @@ class Ventana(QtWidgets.QWidget):
         neu = len(result[result == 2]) #guardamos la cantidad de tweets neutros
         y = [pos, neu, neg] # vector de la cantidad de tweets positivos, negativos y neutros
         return (it[0],y)
+    '''Función que muestra los gráficos utilizando los plots de python'''
     def mostrar_graph(self, NB,SVC, KNN, MLP):
         #Naive Bayes
         plt.subplot(221)
@@ -454,49 +495,28 @@ class Ventana(QtWidgets.QWidget):
         plt.xticks(range(len(MLP[1])), ['positive', 'neutral', 'negative'])
         plt.bar(range(len(MLP[1])), height=MLP[1], width = 0.75, align = 'center', alpha = 0.8)
         plt.show()
-    def mostrar_graph(self, NB,SVC, KNN, MLP):
-        #Naive Bayes
-        plt.subplot(221)
-        plt.title("NB para la entidad " + NB[0])
-        plt.ylabel('tweets')
-        plt.xticks(range(len(NB[1])), ['positive', 'neutral', 'negative'])
-        plt.bar(range(len(NB[1])), height=NB[1], width = 0.75, align = 'center', alpha = 0.8)
-        #SVC
-        plt.subplot(222)
-        plt.title("SVC para la entidad " + SVC[0])
-        plt.ylabel('tweets')
-        plt.xticks(range(len(SVC[1])), ['positive', 'neutral', 'negative'])
-        plt.bar(range(len(SVC[1])), height=SVC[1], width = 0.75, align = 'center', alpha = 0.8)
-        #KNN
-        plt.subplot(223)
-        plt.title("KNN para la entidad " + KNN[0])
-        plt.ylabel('tweets')
-        plt.xticks(range(len(KNN[1])), ['positive', 'neutral', 'negative'])
-        plt.bar(range(len(KNN[1])), height=KNN[1], width = 0.75, align = 'center', alpha = 0.8)
-        #MLP
-        plt.subplot(224)
-        plt.title("MLP para la entidad " + MLP[0])
-        plt.ylabel('tweets')
-        plt.xticks(range(len(MLP[1])), ['positive', 'neutral', 'negative'])
-        plt.bar(range(len(MLP[1])), height=MLP[1], width = 0.75, align = 'center', alpha = 0.8)
-        plt.show()
-    '''
-    Función que utiliza NER para detectar entidades.
-    '''
+    '''Función que utiliza NER para detectar entidades.'''
     def usar_NER(self, tweetys, n):
+        self.progressBarUnTweet.reset()
+        self.progressBarUnTweet.setMaximum(len(tweetys)*10)
+        self.progressBarUnTweet.setMinimum(0)
+        self.progresLabel.setText("ANALIZANDO ENTIDADES")
         st = StanfordNERTagger(r'C:\Users\Servicio Técnico\Documents\stanford-ner-2018-02-27\classifiers\english.all.3class.distsim.crf.ser.gz')
         #acuérdate de que cambia para el mac que es donde vas a realizar la presentación
         entities = []
-
+        tindice = 0
         for r in tweetys:
             print("está analizando(r): ", r)
             lst_tags = st.tag(r) #no tengo que hacer el split porque ya está hecho?
             for tup in lst_tags:
                 print("está analizando(tup): ", tup)
+                self.progressBarUnTweet.setValue(tindice)
+                tindice = tindice + 1
                 if(tup[1] != 'O'):
                     print("mete(tup) ", tup, "en las entidades")
                     entities.append(tup)
         df_entities = pd.DataFrame(entities)
+        self.progressBarUnTweet.setValue(len(tweetys)*10)
         if df_entities.size >0:
             df_entities.columns = ["word","ner"]
             #Organizaciones
@@ -514,11 +534,9 @@ class Ventana(QtWidgets.QWidget):
             return (organizaciones, personas, lugares)
         else:
             return 0
-
-    '''
-    Función que entrena todos los algoritmos utilizando datos de ficheros de entrenamiento
-    y de test. En la misma función se limpian los datos tokenizados.
-    '''
+    '''Función que entrena todos los algoritmos utilizando datos de ficheros de entrenamiento
+    y de test. En la misma función se limpian los datos tokenizados. Al final detecta cuál
+    es la mejor configuración para el algoritmo y los entrena con dicha configuración'''
     def entrenar_algoritmos(self):
         #self.infoLayout.hide()
         self.progressBarUnTweet.reset()
@@ -665,6 +683,8 @@ class Ventana(QtWidgets.QWidget):
         self.progresLabel.setText("FINALIZADO")
         self.entrenamientoLabel.setText("<h1>ENTRENAMIENTO REALIZADO CON ÉXITO</h1>")
         self.layoutWidget.addWidget(self.entrenamientoLabel)
+    '''Función auxiliar para usar_NER que guarda los archivos de entrenamiento para que
+    se guarden en futuras sesiones'''
     def entrenar(self, alg,train_vectors,train_labels,test_vectors, test_labels, mod):
         nfile = alg
         if path.exists(nfile):
@@ -708,13 +728,5 @@ def main():
     ventana.show()
     sys.exit(app.exec_())       # [JMTORRES] La ejecución no sale de exec() hasta que la ventana principal se cierra.
 
-    
 if __name__ == '__main__':
-    # [JMTORRES] por el if esto sólo se ejecutará si llamamos al script como un programa
-    # en plan:
-    #       ./qt_script.py
-    # o
-    #   python qt_script.py
-    # Nunca se llamará a main() si se intenta hacer un import qt_script desde otro programa
-    # en python.
       main()
